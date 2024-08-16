ea176ada4c9fc7c53b56a228556780700e2bd5ac
Dave Lanning <davel@cpanel.net>
2024-08-16 02:50:03 +0000

    Converting some of the POC code over to be cleaner.  Also reverted to using prove since yath isn't in a typical VM until we do the build which we can decide on later.

--- .github/workflows/openstack/status_marker.sh
+++ .github/workflows/openstack/status_marker.sh
@@ -10,8 +10,8 @@ RC=$(echo ${RELEASE_INFO} | wc -c)
 
 for ((i=1; i<=${RC}+17; i++)); do echo -n "#"; done
 echo;
-echo "# STAGE: ${STAGE} of 5                                          #";
+echo "# STAGE: ${STAGE} of 5                                           #";
 echo "# OS Release: ${RELEASE_INFO}   #";
-echo "# cP Version: ${CPANEL_VERSION}                              #";
+echo "# cP Version: ${CPANEL_VERSION}                               #";
 for ((i=1; i<=${RC}+17; i++)); do echo -n "#"; done
 echo;
